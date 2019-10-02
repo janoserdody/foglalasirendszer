6e8203772ae4855d66f2c588f8a240ecfcded366
janoserdody <37834280+janoserdody@users.noreply.github.com>
2019-10-02 23:38:26 +0200

    Draft Booking
    vázlatosan elkészült a főmenü és a 2. form, a "foglalás bevitele"
    Már csak a felhasználói felületet kell megtervezni.

--- ViewControlLayer/src/viewcontrol/App.java
+++ ViewControlLayer/src/viewcontrol/App.java
@@ -1,6 +1,7 @@
 package viewcontrol;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -9,12 +10,33 @@ public class App {
     private JPanel panelMain;
 
     public App() {
+        AddButtonAction();
+
+        SetMainView();
+    }
+
+    private void AddButtonAction() {
         buttonMsg.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(null, "Hello world");
+                DisplayFoglalasBevitele();
             }
         });
+    }
+
+    private void SetMainView() {
+        Font f = new Font("sans-serif", Font.PLAIN, 20);
+        UIManager.put("Menu.font", f);
+        UIManager.put("MenuItem.font", f);
+    }
+
+    private static void DisplayFoglalasBevitele() {
+        JFrame foglalas = new JFrame("Foglalás");
+        foglalas.setContentPane(new Foglalas().foglalasPanel);
+        foglalas.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+        foglalas.pack();
+        foglalas.setVisible(true);
+        foglalas.setSize(500,500);
     }
 
     public static void main(String[] args) {
@@ -23,6 +45,22 @@ public class App {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
-        
+
+        JMenuBar menubar = new JMenuBar();
+        JMenu menu = new JMenu("Főmenü");
+        menu.setSize(800,20);
+        JMenuItem menuItemFoglalas = new JMenuItem("Foglalás bevitele");
+        menuItemFoglalas.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                DisplayFoglalasBevitele();
+            }
+        });
+
+        menu.add(menuItemFoglalas);
+        menubar.add(menu);
+
+        frame.setJMenuBar(menubar);
+        frame.setSize(1000,800);
     }
 }
---
+++ ViewControlLayer/src/viewcontrol/Foglalas.java
@@ -0,0 +1,13 @@
+package viewcontrol;
+
+import javax.swing.*;
+
+public class Foglalas {
+    private JLabel labelFoglalas;
+    public JPanel foglalasPanel;
+
+    public Foglalas(){
+
+
+    }
+}
