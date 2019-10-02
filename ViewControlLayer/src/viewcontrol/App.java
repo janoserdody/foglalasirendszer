15ab0cfe7863d4dddac430c2d42fe0849e581340
janoserdody <37834280+janoserdody@users.noreply.github.com>
2019-10-02 22:52:04 +0200

    first form

---
+++ ViewControlLayer/src/viewcontrol/App.java
@@ -0,0 +1,28 @@
+package viewcontrol;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class App {
+    private JButton buttonMsg;
+    private JPanel panelMain;
+
+    public App() {
+        buttonMsg.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, "Hello world");
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("App");
+        frame.setContentPane(new App().panelMain);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+        
+    }
+}
