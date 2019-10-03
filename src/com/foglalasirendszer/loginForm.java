1811a324f0d2e49cdb63d6e26832e85652a7fefb
AtT85 <tothattila@rocketmail.com>
2019-10-03 20:03:06 +0200

    Update login form
    login form regisztrációs része, nagyon alap designal

--- src/com/foglalasirendszer/loginForm.java
+++ src/com/foglalasirendszer/loginForm.java
@@ -7,10 +7,8 @@ public class loginForm {
     private JPanel panel1;
 
     public static void main(String[] args) {
-        JFrame frame=new JFrame("foglalasirendszer");
-        frame.setContentPane(new loginForm().panel1);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
+        loginClass login=new loginClass();
+
+
     }
 }
