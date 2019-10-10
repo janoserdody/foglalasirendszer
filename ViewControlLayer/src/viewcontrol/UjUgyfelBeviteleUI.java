c613b3a58fbb19418aa322c5e98ae64b5fe7e3f2
AtT85 <tothattila@rocketmail.com>
2019-10-10 14:10:00 +0200

    Update új ügyfél hozzáadása form

--- ViewControlLayer/src/viewcontrol/UjUgyfelBeviteleUI.java
+++ ViewControlLayer/src/viewcontrol/UjUgyfelBeviteleUI.java
@@ -23,6 +23,7 @@ public class UjUgyfelBeviteleUI {
     private JTextField emailTextfield;
     private JLabel telefonLabel;
     private JTextField telefonTextfield;
+    private JButton keszButton;
 
     private JPanel megszPanel;
     private JPanel vezPanel;
@@ -32,51 +33,70 @@ public class UjUgyfelBeviteleUI {
         this.framework = framework;
         ujUgyfel = new JFrame("Új ügyfél bevitele");
         ujUgyfelBevitelePanel=new JPanel();
+        ujUgyfel.getContentPane();
+        //ujUgyfelBevitelePanel.setLayout(new FlowLayout());
+        //ujUgyfel.getContentPane().add(ujUgyfelBevitelePanel,"Center");
+        ujUgyfelBevitelePanel.setLayout(null);
 
+        Font adatokFont=new Font("Arial",Font.BOLD,20);
+
+        Dimension cimSize=new Dimension();
+        cimSize.setSize(400,35);
+        Dimension adatokSize=new Dimension();
+        adatokSize.setSize(350,25);
 
 
         title=new JLabel("Új ügyfél hozzáadása");
-        title.setBounds(600,20,100,25);
-        title.setFont(new Font("Arial",Font.BOLD,25));
+        title.setBounds(350,20,cimSize.width,cimSize.height);
+        title.setFont(new Font("Arial",Font.BOLD,30));
+
 
         megszolitasLabel=new JLabel("Megszólítás kiválasztása:");
-        megszolitasLabel.setBounds(20,60,100,25);
+        megszolitasLabel.setBounds(30,110,adatokSize.width,adatokSize.height);
+        megszolitasLabel.setFont(adatokFont);
 
         jComboBox=new JComboBox();
         jComboBox.addItem("id");
         jComboBox.addItem("ifj");
         jComboBox.addItem("dr");
-        jComboBox.setBounds(160,60,100,25);
+        jComboBox.setBounds(400,110,50,adatokSize.height);
 
         vezetekNevLabel=new JLabel("Vezetéknév:");
-        vezetekNevLabel.setBounds(20,90,100,25);
+        vezetekNevLabel.setBounds(30,160,adatokSize.width,adatokSize.height);
+        vezetekNevLabel.setFont(adatokFont);
+
         vezetekNevTextfield=new JTextField();
-        vezetekNevTextfield.setBounds(160,90,100,25);
-        vezetekNevTextfield.setToolTipText("Set last name");
+        vezetekNevTextfield.setBounds(400,160,adatokSize.width,adatokSize.height);
 
         keresztNevLabel=new JLabel("Keresztnév:");
+        keresztNevLabel.setBounds(30,210,adatokSize.width,adatokSize.height);
+        keresztNevLabel.setFont(adatokFont);
 
         keresztNevTextfield=new JTextField();
-        keresztNevTextfield.setToolTipText("Set first name");
+        keresztNevTextfield.setBounds(400,210,adatokSize.width,adatokSize.height);
 
         emailLabel=new JLabel("E-mail:");
+        emailLabel.setBounds(30,260,adatokSize.width,adatokSize.height);
+        emailLabel.setFont(adatokFont);
+
         emailTextfield=new JTextField();
+        emailTextfield.setBounds(400,260,adatokSize.width,adatokSize.height);
 
         telefonLabel=new JLabel("Telefon:");
+        telefonLabel.setBounds(30,310,adatokSize.width,adatokSize.height);
+        telefonLabel.setFont(adatokFont);
+
         telefonTextfield=new JTextField();
+        telefonTextfield.setBounds(400,310,adatokSize.width,adatokSize.height);
 
-        //megszPanel=new JPanel();
-        //megszPanel.add(megszolitasLabel);
-        //megszPanel.add(jComboBox);
+        keszButton=new JButton("Ügyfél hozzáadása");
+        keszButton.setBounds(350,500,280,40);
 
-        //vezPanel=new JPanel();
-        //vezPanel.add(vezetekNevLabel);
-        //vezPanel.add(vezetekNevTextfield);
+
 
         ujUgyfelBevitelePanel.add(title);
         ujUgyfelBevitelePanel.add(megszolitasLabel);
         ujUgyfelBevitelePanel.add(jComboBox);
-
         ujUgyfelBevitelePanel.add(vezetekNevLabel);
         ujUgyfelBevitelePanel.add(vezetekNevTextfield);
         ujUgyfelBevitelePanel.add(keresztNevLabel);
@@ -85,6 +105,9 @@ public class UjUgyfelBeviteleUI {
         ujUgyfelBevitelePanel.add(emailTextfield);
         ujUgyfelBevitelePanel.add(telefonLabel);
         ujUgyfelBevitelePanel.add(telefonTextfield);
+        ujUgyfelBevitelePanel.add(keszButton);
+
+        ujUgyfel.add(ujUgyfelBevitelePanel);
     }
 
     public JFrame getUjUgyfel() {
